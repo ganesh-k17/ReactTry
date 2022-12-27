c713cb4f4fbcf6e702d8cb74646d02ac59ba486c
ganeshk <ganku007@live.in>
2022-12-27 19:16:25 +0530

    Update counter.jsx

--- src/app4/src/Component/counter.jsx
+++ src/app4/src/Component/counter.jsx
@@ -2,54 +2,25 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
-    state = { 
-        value: this.props.value,
-        tags: ['tag1','tag2','tag3']
-     } 
-
     render() { 
 
         console.log("props",this.props);
 
         let classes = "badge m-2 bg-"
-        classes += (this.state.value === 0) ? "warning" : "primary";
+        classes += (this.props.value === 0) ? "warning" : "primary";
 
         return (
-            <React.Fragment>
+            <div>
                 <span style={ { fontSize:25, backgroundColor: 'blue' } } className={classes}>{this.getCount()}</span>
-                <span><button onClick={ this.handleIncrement } className="btn btn-secondary btn-sm m-2">Increment</button></span>
-                <span><button onClick={ this.handleDecrement } className="btn btn-secondary btn-sm m-2">Decrement</button></span>
-                <span><button onClick={() => this.props.onDeleted(this.props.id) } className="btn btn-danger btn-sm m-2">Delete</button></span>
-                <ul>
-                    { this.renderTags() }
-                </ul>
-            </React.Fragment>
+                <span><button onClick={ () => this.props.onIncremented(this.props.value) } className="btn btn-secondary btn-sm m-2">Increment</button></span>
+                <span><button onClick={ () => this.props.onDecremented(this.props.value) } className="btn btn-secondary btn-sm m-2">Decrement</button></span>
+                <span><button onClick={() => this.props.onDeleted(this.props.value) } className="btn btn-danger btn-sm m-2">Delete</button></span>
+            </div>
         );
     }
-
-    renderTags(){
-        let tags = this.state.tags;
-        if (tags.length === 0)
-            return <p>No tags available"</p>
-        return tags.map(tag => <li key={ tag }><button onClick={ () => this.handleTag(tag)}>tag:{tag}</button></li>)
-    }
-
-    handleTag = (tag) => {
-        console.log(tag);
-    }
-
-    handleIncrement = () => {
-        this.setState({ value: this.state.value + 1 });
-        console.log("incremented", this);
-    }
-
-    handleDecrement = () => {
-        this.setState({ value: this.state.value - 1  });
-        console.log("incremented", this);
-    }
-
+    
     getCount(){
-        const { value: Count } = this.state;
+        const { value: Count } = this.props.value;
         return Count === 0 ? "Zero" : Count;
     }
 }
