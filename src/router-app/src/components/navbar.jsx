b2c63dba9e26e72ce8170dbaada86c63be1fdd15
ganeshk <4047354+ganesh-k17@users.noreply.github.com>
2023-01-04 19:21:17 +0530

    Update navbar.jsx

--- src/router-app/src/components/navbar.jsx
+++ src/router-app/src/components/navbar.jsx
@@ -1,22 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <ul>
       <li>
-        <a href="/">Home</a>
+        <Link to="/">Home</Link>
       </li>
       <li>
-        <a href="/products">Products</a>
+        <Link to="/products">Products</Link>
       </li>
       <li>
-        <a href="/posts/2018/06">Posts</a>
+        <Link to="/posts/2018/06">Posts</Link>
       </li>
       <li>
-        <a href="/admin">Admin</a>
+        <Link to="/admin">Admin</Link>
       </li>
     </ul>
   );
 };
 
 export default NavBar;
+
+
+
+// import React from "react";
+
+// const NavBar = () => {
+//   return (
+//     <ul>
+//       <li>
+//         <a href="/">Home</a>
+//       </li>
+//       <li>
+//         <a href="/products">Products</a>
+//       </li>
+//       <li>
+//         <a href="/posts/2018/06">Posts</a>
+//       </li>
+//       <li>
+//         <a href="/admin">Admin</a>
+//       </li>
+//     </ul>
+//   );
+// };
+
+// export default NavBar;
